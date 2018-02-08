7b50bcf2d66a8a073c89fde61db6a36300a843cf
Mustafa Genç <eposta@mustafagenc.info>
2018-02-08 16:22:08 +0300

    Türkçe dil dosyası eklendi.
    Gereksiz dil dosyaları silindi.

---
+++ resources/lang/tr/admin.php
@@ -0,0 +1,112 @@
+<?php
+
+return [
+    'online'                => 'Çevirimiçi',
+    'login'                 => 'Giriş Yap',
+    'logout'                => 'Çıkış Yap',
+    'setting'               => 'Ayarlar',
+    'name'                  => 'İsim',
+    'username'              => 'Kullanıcı Adı',
+    'password'              => 'Parola',
+    'password_confirmation' => 'Parola Doğrulama',
+    'remember_me'           => 'Beni Hatırla',
+    'user_setting'          => 'Kullanıcı Ayarları',
+    'avatar'                => 'Profil Fotoğrafı',
+
+    'list'         => 'Liste',
+    'new'          => 'Yeni',
+    'create'       => 'Yeni',
+    'delete'       => 'Sil',
+    'remove'       => 'Temizle',
+    'edit'         => 'Düzenle',
+    'view'         => 'Görüntüle',
+    'browse'       => 'Gözat',
+    'reset'        => 'Temizle',
+    'export'       => 'Dışarı Aktar',
+    'batch_delete' => 'Toplu Sil',
+    'save'         => 'Kaydet',
+    'refresh'      => 'Yenile',
+    'order'        => 'Sırala',
+    'expand'       => 'Genişlet',
+    'collapse'     => 'Daralt',
+    'filter'       => 'Filtre',
+    'close'        => 'Kapat',
+    'show'         => 'Göster',
+    'entries'      => 'kayıtlar',
+    'captcha'      => 'Captcha',
+
+    'action'            => 'İşlem',
+    'title'             => 'Başlık',
+    'description'       => 'Açıklama',
+    'back'              => 'Geri Dön',
+    'back_to_list'      => 'Listeye Dön',
+    'submit'            => 'Gönder',
+    'menu'              => 'Menü',
+    'input'             => 'Giriş',
+    'succeeded'         => 'Başarılı',
+    'failed'            => 'Başarısız oldu',
+    'delete_confirm'    => 'Bu kaydı silmek istediğinizden emin misiniz?',
+    'delete_succeeded'  => 'Öğe başarılı bir şekilde silindi!',
+    'delete_failed'     => 'Öğre silinirken bir hata oluştu!',
+    'update_succeeded'  => 'Öğe başarılı bir şekilde güncellendi!',
+    'save_succeeded'    => 'Başarılı bir şekilde kaydedildi!',
+    'refresh_succeeded' => 'Yenileme başarılı!',
+    'login_successful'  => 'Giriş başarılı',
+
+    'choose'       => 'Gözat',
+    'choose_file'  => 'Dosya Seç',
+    'choose_image' => 'Resim Seç',
+
+    'more' => 'devamı',
+    'deny' => 'Bunu yapmaya izniniz yok.',
+
+    'administrator' => 'Yönetici Listesi',
+    'roles'         => 'Roller',
+    'permissions'   => 'İzinler',
+    'slug'          => 'Url',
+
+    'created_at' => 'Eklendi',
+    'updated_at' => 'Güncellendi',
+
+    'alert' => 'Uyarı',
+
+    'parent_id' => 'Temel',
+    'icon'      => 'Ikon',
+    'uri'       => 'URL',
+
+    'operation_log'       => 'Kullanıcı Günlükleri',
+    'parent_select_error' => 'Hata',
+
+    'pagination' => [
+        'range' => 'Görüntülenen :first - :last toplam :total sayfa',
+    ],
+
+    'role'       => 'Rol',
+    'permission' => 'İzin',
+    'route'      => 'Yok',
+    'confirm'    => 'Doğrula',
+    'cancel'     => 'İptal Et',
+
+    'http' => [
+        'method' => 'HTTP method',
+        'path'   => 'HTTP path',
+    ],
+    'all_methods_if_empty' => 'Tüm metodlar boşsa',
+
+    'all'           => 'Tümü',
+    'current_page'  => 'Mevcut Sayfa',
+    'selected_rows' => 'Seçilenler',
+
+    'upload'     => 'Yükle',
+    'new_folder' => 'Yeni Klasör',
+    'time'       => 'Zaman',
+    'size'       => 'Boyut',
+
+    'listbox' => [
+        'text_total'         => 'Tümü Gösteriliyor {0}',
+        'text_empty'         => 'Boş Liste',
+        'filtered'           => '{0} / {1}',
+        'filter_clear'       => 'Tümünü Göster',
+        'filter_placeholder' => 'Filtre',
+    ],
+];
